c847dbe53174be0448faa7d63c882015a2071138
berlianamartin <berlianadewintamartin@gmail.com>
2023-01-06 08:44:13 +0700

    Praktek PBO Form

---
+++ Berli/controller/PeminjamanController.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Berli.controller;
+import Berli.dao.PeminjamanDaoImpl;
+import Berli.dao.AnggotaDao;
+import Berli.dao.AnggotaDaoImpl;
+import Berli.dao.BukuDao;
+import Berli.dao.BukuDaoImpl;
+import Berli.dao.PeminjamanDao;
+import Berli.model.Peminjaman;
+import Berli.model.Anggota;
+import Berli.model.Buku;
+import Berli.view.FormPeminjaman;
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+/**
+ *
+ * @author USER
+ */
+public class PeminjamanController {
+    private FormPeminjaman formPeminjaman;
+    private AnggotaDao anggotaDao;
+    private BukuDao bukuDao;
+    private PeminjamanDao peminjamanDao;
+    private Peminjaman peminjaman;
+    
+    
+    public PeminjamanController (FormPeminjaman formPeminjaman){
+        this.formPeminjaman = formPeminjaman;
+        peminjamanDao = new PeminjamanDaoImpl();
+        anggotaDao = new AnggotaDaoImpl();
+        bukuDao = new BukuDaoImpl();
+    }
+    public void bersihForm(){
+        formPeminjaman.getTxtTglpinjam().setText("");
+        formPeminjaman.getTxtTglkembali().setText("");
+        
+    }
+    
+    public void setCboNobp(){
+        formPeminjaman.getCboNobp().removeAllItems();
+        List<Anggota> list = anggotaDao.getAllAnggota();
+        for (Anggota anggota : list) {
+            formPeminjaman.getCboNobp().
+                addItem(anggota.getNobp()+"-"+anggota.getNama());
+        }  
+    }
+    
+     public void setCboBuku(){
+        formPeminjaman.getCboBuku().removeAllItems();
+        List<Buku> list = bukuDao.getAll();
+        for (Buku buku : list) {
+            formPeminjaman.getCboBuku().
+                addItem(buku.getKodeBuku());
+        }   
+    }
+    
+    public void savePeminjaman(){
+        peminjaman = new Peminjaman();
+        peminjaman.setNobp(formPeminjaman.getCboNobp()
+                .getSelectedItem().toString().split("-")[0]);
+        peminjaman.setKodebuku( formPeminjaman.getCboBuku()
+                .getSelectedItem().toString());
+        peminjaman.setTglpinjam( formPeminjaman.getTxtTglpinjam().getText());
+        peminjaman.setTglkembali( formPeminjaman.getTxtTglkembali().getText());
+        peminjamanDao.save(peminjaman);
+        javax.swing.JOptionPane.showMessageDialog(formPeminjaman,"Entri Ok");
+    }
+    
+    public void getPeminjaman(){
+        int index = formPeminjaman.getTblPeminjaman().getSelectedRow();
+        peminjaman = peminjamanDao.getPeminjaman(index);
+        if (peminjaman != null){
+            List<Anggota> listAnggota = anggotaDao.getAllAnggota();
+            for(Anggota anggota:listAnggota){
+                if(peminjaman.getNobp()==anggota.getNobp()){
+                    formPeminjaman.getCboNobp()
+                    .setSelectedItem(anggota.getNobp()+"-"+anggota.getNama());
+                    break;         
+                }
+            }
+            formPeminjaman.getCboBuku().setSelectedItem(peminjaman.getKodebuku());
+            formPeminjaman.getTxtTglpinjam().setText(peminjaman.getTglpinjam());
+            formPeminjaman.getTxtTglkembali().setText(peminjaman.getTglkembali());
+        }
+    }
+    
+    public void updatePeminjaman(){
+        int index = formPeminjaman.getTblPeminjaman().getSelectedRow();
+        peminjaman = peminjamanDao.getPeminjaman(index);
+         peminjaman.setNobp(formPeminjaman.getCboNobp()
+                .getSelectedItem().toString().split("-")[0]);
+        peminjaman.setKodebuku( formPeminjaman.getCboBuku()
+                .getSelectedItem().toString());
+        peminjaman.setTglpinjam( formPeminjaman.getTxtTglpinjam().getText());
+        peminjaman.setTglkembali( formPeminjaman.getTxtTglkembali().getText());
+        peminjamanDao.save(peminjaman);
+        javax.swing.JOptionPane.showMessageDialog(formPeminjaman,"Update Ok");
+    }
+    
+    public void deletePeminjaman(){
+         int index = formPeminjaman.getTblPeminjaman().getSelectedRow();
+         peminjamanDao.delete(index);
+         javax.swing.JOptionPane.showMessageDialog(formPeminjaman,"Delete Ok");
+    }
+    public void tampilData(){
+        DefaultTableModel tabelModel = 
+                (DefaultTableModel)formPeminjaman.getTblPeminjaman().getModel();
+        tabelModel.setRowCount(0);
+        java.util.List<Peminjaman> list = peminjamanDao.getAll();
+        for (Peminjaman peminjaman : list){
+            Object[] data = {
+                peminjaman.getNobp(),
+                peminjaman.getKodebuku(),
+                peminjaman.getTglpinjam(),
+                peminjaman.getTglkembali()
+            };
+            tabelModel.addRow(data);
+        }
+    }
+}
