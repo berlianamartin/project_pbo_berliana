b69c9d8e6e6045fd07d663fae5075d5657659c54
berlianamartin <berlianadewintamartin@gmail.com>
2023-01-06 09:34:52 +0700

    Berli - Pertemuan Form

---
+++ Praktek PBO - Berliana Dewinta Martin - 2101092003/Berli/dao/AnggotaDao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Berli.dao;
+import Berli.model.Anggota;
+import java.util.List;
+/**
+ *
+ * @author USER
+ */
+public interface AnggotaDao {
+    Anggota save(Anggota anggota);
+    Anggota update(int index, Anggota anggota);
+    Anggota delete(int index);
+    Anggota getAnggota(int index);
+    List<Anggota> getAllAnggota();
+    
+}
