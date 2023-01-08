586bcda3c55e0e57dff961424bc34f004cf09a88
berlianamartin <berlianadewintamartin@gmail.com>
2023-01-08 17:06:05 +0700

    Satu

---
+++ Praktek PBO - Berliana Dewinta Martin - 2101092003/satu/Contoh1Example.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package satu;
+
+/**
+ *
+ * @author USER
+ */
+public class Contoh1Example {
+    public static void main(String[] args) {
+        contoh1 c = new contoh1();
+        c.a = 10;
+    }
+}
---
+++ Praktek PBO - Berliana Dewinta Martin - 2101092003/satu/contoh1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package satu;
+
+/**
+ *
+ * @author USER
+ */
+public class contoh1 {
+    public int a;
+    public int getA(){
+        return a;
+    }
+}
