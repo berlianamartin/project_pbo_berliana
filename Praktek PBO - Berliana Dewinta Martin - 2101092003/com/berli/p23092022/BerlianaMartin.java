e946187e5fb248f74a15901ade2a6ecc9c8b9e65
berlianamartin <berlianadewintamartin@gmail.com>
2023-01-08 17:05:21 +0700

    Com Berliana

---
+++ Praktek PBO - Berliana Dewinta Martin - 2101092003/com/berli/p23092022/BerlianaMartin.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.berli.p23092022;
+
+/**
+ *
+ * @author USER
+ */
+public class BerlianaMartin {
+     public static void main(String[] args) {
+     System.out.println("Welcome To Java Programming BERLIANA DEWINTA MARTIN");
+    }
+}
