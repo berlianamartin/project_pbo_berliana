0e8bfc40428011cb1209cd92d48df25f9787d622
berlianamartin <berlianadewintamartin@gmail.com>
2023-01-08 16:59:26 +0700

    Pertemuan 16122022

---
+++ Praktek PBO - Berliana Dewinta Martin - 2101092003/Berli16122022/ContohException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Berli16122022;
+
+/**
+ *
+ * @author USER
+ */
+public class ContohException {
+    public static void main(String[] args) {
+        try {
+            int a = 1/10;
+            system.out.println("Hasil a = "+a);      
+        }catch(java.lang.ArithmeticException ex){
+        system.out.println("Erro "+ex.getMessage());
+        }
+    }
+}
+
